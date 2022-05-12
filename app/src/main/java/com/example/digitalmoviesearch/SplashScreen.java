c69e5be96be1408e3ad4abf3c70c2d85714a4cc3
Harsh Verma <harsh1512verma@gmail.com>
2022-05-12 18:45:48 +0530

    update screensplash

--- app/src/main/java/com/example/digitalmoviesearch/SplashScreen.java
+++ app/src/main/java/com/example/digitalmoviesearch/SplashScreen.java
@@ -2,13 +2,27 @@ package com.example.digitalmoviesearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.digitalmoviesearch.databinding.ActivitySplashScreenBinding;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ActivitySplashScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },1500);
     }
 }
